18c7a638c091bfe72fdac69b02b1c0d3792cb2db
Kinga Wenta <kinga.wenta@gmail.com>
2021-05-24 21:35:23 +0200

    creator

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,17 +3,24 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator';
 
 
 class List extends React.Component {
+  state = {
+    columns: this.props.columns || [],
+  }
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node.isRequired,
+    description: PropTypes.node,
+    columns: PropTypes.array,
     image: PropTypes.string,
   }
 
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>,
+    description: settings.defaultListDescription,
   }
 
   render() {
@@ -21,12 +28,15 @@ class List extends React.Component {
       <section className={styles.component}>
         <Hero titleText={this.props.title} imageImg={this.props.image} />
         <div className={styles.description}>
-          {this.props.children}
+          {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
-          <Column columnTitle={'Animals'}></Column>
-          <Column columnTitle={'Plants'}></Column>
-          <Column columnTitle={'Minerals'}></Column>
+          {this.state.columns.map(({key, ...columnProps}) => (
+          <Column key={key} {...columnProps} />
+          ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
         </div>
       </section>
     )
