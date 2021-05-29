0dc0833c80a0067f0df9ce5c22a224bb7e59e014
Kinga Wenta <kinga.wenta@gmail.com>
2021-05-29 16:01:52 +0200

    repair

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -37,8 +37,8 @@ class Column extends React.Component {
           <Card key={key} {...cardProps} />
         ))}  
 
-        <div className={styles.Creator}>
-        <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+        <div className={styles.creator}>
+        <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
         </div> 
       </section>
     )
