34e2ae5e177709424c8e63b6722cdec58cc803a0
Kinga Wenta <kinga.wenta@gmail.com>
2021-06-12 21:16:56 +0200

    searchresult

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -2,30 +2,20 @@ import React from 'react';
 import styles from './SearchResults.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card';
-import { settings } from '../../data/dataStore';
-import Icon from '../Icon/Icon';
 import Container from '../Container/Container';
 
 class SearchResults extends React.Component {
   
   static propTypes = {
-    title: PropTypes.node,
     cards: PropTypes.array,
-    icon: PropTypes.node,
-  }
-
-  static defaultProps = {
-    icon: settings.defaultColumnIcon,
   }
  
   render() {
-    const {title, icon, cards} = this.props;
+    const {cards} = this.props;
     
     return (
       <Container>
         <section className={styles.component}>
-          <h3 className={styles.title}>{title}<span className={styles.icon}><Icon name={icon}></Icon></span></h3>
-
           <div className={styles.cards}>
             {cards.map(cardData => (
               <Card key={cardData.id} {...cardData} />
